5e50c8059dbbe0fa2b50fc80c7df2b03918f7650
maciekz1996 <maciek3609@gmail.com>
2020-09-06 19:04:02 +0200

    Updates

---
+++ precache-manifest.0fa7db263c32d3d2f63f9f84640e711e.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f4570eb56e1e2a1707a3e35d2a91aa",
+    "url": "/new-portfolio/index.html"
+  },
+  {
+    "revision": "144c14466a6146e53e2f",
+    "url": "/new-portfolio/static/css/main.e0459923.chunk.css"
+  },
+  {
+    "revision": "a2209081222562377e06",
+    "url": "/new-portfolio/static/js/2.68baf1f9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/new-portfolio/static/js/2.68baf1f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "144c14466a6146e53e2f",
+    "url": "/new-portfolio/static/js/main.d3dde0c0.chunk.js"
+  },
+  {
+    "revision": "cf11cfd6abd9c0692dac",
+    "url": "/new-portfolio/static/js/runtime-main.ef628a11.js"
+  },
+  {
+    "revision": "460d9424c4aaa07f5c919543669b752d",
+    "url": "/new-portfolio/static/media/ballin.460d9424.png"
+  },
+  {
+    "revision": "87b0dd6159ec9cc59d1c69db910e3af8",
+    "url": "/new-portfolio/static/media/bg-contact-page.87b0dd61.svg"
+  },
+  {
+    "revision": "6e09b4a7bed3a4ed39e629eb40bb548d",
+    "url": "/new-portfolio/static/media/bg-mobile-projects-page.6e09b4a7.svg"
+  },
+  {
+    "revision": "ef5de22bc9b7c8ca26b429eb4beb2951",
+    "url": "/new-portfolio/static/media/bg-mobile-skills-page.ef5de22b.svg"
+  },
+  {
+    "revision": "bc332886113165cd87dc254251eb2aa0",
+    "url": "/new-portfolio/static/media/bg-projects-page.bc332886.svg"
+  },
+  {
+    "revision": "b65cdf0a6abd8e7667c39e99ab646ab6",
+    "url": "/new-portfolio/static/media/bg-skills-page.b65cdf0a.svg"
+  },
+  {
+    "revision": "68e6bb2f3d3759e3ced1fe6da6f4c426",
+    "url": "/new-portfolio/static/media/bootstrap.68e6bb2f.svg"
+  },
+  {
+    "revision": "40934c90b197bcf859b37cbedd616603",
+    "url": "/new-portfolio/static/media/css3.40934c90.svg"
+  },
+  {
+    "revision": "a56434e034e06b782f83501dbd675839",
+    "url": "/new-portfolio/static/media/firebase.a56434e0.svg"
+  },
+  {
+    "revision": "6885c6c5f26657ab9cf4486fa28c416c",
+    "url": "/new-portfolio/static/media/flutter.6885c6c5.svg"
+  },
+  {
+    "revision": "1d253773ae583b713653c7ebb1d3df18",
+    "url": "/new-portfolio/static/media/gymino-app.1d253773.png"
+  },
+  {
+    "revision": "15ec172c85fdbcde6ee396d9dca3306e",
+    "url": "/new-portfolio/static/media/gymino-dashboard.15ec172c.png"
+  },
+  {
+    "revision": "0fd4cdb8741e516e396515b0b73c4fe1",
+    "url": "/new-portfolio/static/media/html5_icon.0fd4cdb8.svg"
+  },
+  {
+    "revision": "9a34504f529909bedddcf8e9b0fecea7",
+    "url": "/new-portfolio/static/media/icon-email.9a34504f.svg"
+  },
+  {
+    "revision": "a0f74230d6c19e250fc842285cdf0d31",
+    "url": "/new-portfolio/static/media/icon-phone.a0f74230.svg"
+  },
+  {
+    "revision": "c7273b443e2a74515472fa65eae1f673",
+    "url": "/new-portfolio/static/media/instagram_clone.c7273b44.png"
+  },
+  {
+    "revision": "78e5facecd4ee8b4628fa589b0cb3eaa",
+    "url": "/new-portfolio/static/media/javascript-1.78e5face.svg"
+  },
+  {
+    "revision": "4f3f1391fa2f6b39320de6d326fd22d4",
+    "url": "/new-portfolio/static/media/laravel-brands.4f3f1391.svg"
+  },
+  {
+    "revision": "80d0d0c95e445cc058d295e4d4521f61",
+    "url": "/new-portfolio/static/media/materialize.80d0d0c9.svg"
+  },
+  {
+    "revision": "1f40d7752c817ca9622055dc94a6d649",
+    "url": "/new-portfolio/static/media/polskalokalna.1f40d775.png"
+  },
+  {
+    "revision": "7615def288969e5763ea61878a0d6665",
+    "url": "/new-portfolio/static/media/react-brands.7615def2.svg"
+  },
+  {
+    "revision": "04f21a1dabf631576e4ffc496644adf5",
+    "url": "/new-portfolio/static/media/redux_logo.04f21a1d.svg"
+  },
+  {
+    "revision": "b3f76e597683edaca4db8acc1ecb25a8",
+    "url": "/new-portfolio/static/media/sass.b3f76e59.svg"
+  },
+  {
+    "revision": "405212787dc0f26909d1ca0c80a321c5",
+    "url": "/new-portfolio/static/media/scudetto.40521278.png"
+  },
+  {
+    "revision": "653e8ad2399b3dba4c7dad6143cc5fb7",
+    "url": "/new-portfolio/static/media/sorting-visualizer.653e8ad2.png"
+  },
+  {
+    "revision": "e665bc03ffd5d2a5cce4be7229b850e4",
+    "url": "/new-portfolio/static/media/vue.e665bc03.svg"
+  },
+  {
+    "revision": "0ebec52fb796f992d8db70a6d61b916c",
+    "url": "/new-portfolio/static/media/woocommerce.0ebec52f.svg"
+  },
+  {
+    "revision": "36e6496baa0261b0d71d110ef544ebd8",
+    "url": "/new-portfolio/static/media/wordpress-blue.36e6496b.svg"
+  }
+]);
